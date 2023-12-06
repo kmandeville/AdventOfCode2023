c9aed6d4745e9137040191ee6a314627619ec994
Kevin Mandeville <kmandeville@techtarget.com>
2023-12-06 17:49:30 -0500

    Separated out Day code into packages separate from Main

---
+++ src/main/java/me/kevinmandeville/day2/Day2.java
@@ -0,0 +1,15 @@
+package me.kevinmandeville.day2;
+
+import me.kevinmandeville.Day;
+
+/**
+ * @author kmandeville
+ * @since NEXT_RELEASE_VERSION
+ */
+public class Day2 implements Day {
+
+    @Override
+    public void execute() {
+        
+    }
+}
